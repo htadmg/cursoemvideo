e4a12b0cd63a2e690a636276065367e836cd1a67
htadmg <agatadominguestec@hotmail.com>
2023-04-19 11:47:41 -0300

    Initial commit

---
+++ ex05.py
@@ -0,0 +1,4 @@
+n = int(input('Digite um número:'))
+a = n - 1
+s = n + 1
+print('Analisando o valor {}, o seu Antecessor é {} e o seu sucessor é {}'.format(n, a, s))
---
+++ ex06.py
@@ -0,0 +1,8 @@
+n = int(input('Digite um número:'))
+m = n * 2
+m3 = n * 3
+p = n ** (1/2)
+print('o dobro de {} vale {}.'.format(n, m))
+print('o Triplo de {} vale {}.'.format(n, m3))
+print('A raiz quadrada de {} é igual a {:.1f}.'.format(n, p))
+
